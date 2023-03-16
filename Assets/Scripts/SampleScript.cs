3c5819dcd6be29f5618ea2f31b06cc0b16d6ce72
khlee <khlee@1thefull.com>
2023-03-16 18:37:09 +0900

    add buttons using code

--- Assets/Scripts/SampleScript.cs
+++ Assets/Scripts/SampleScript.cs
@@ -7,14 +7,17 @@ public class SampleScript : MonoBehaviour
 {
     [SerializeField] private Button _button;
 
+    private AndroidJavaObject mViewController;
+
     private void Awake()
     {
+        AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+        mViewController = new AndroidJavaObject("com.khlee.androidview.ViewController", unityActivity);
+
         _button.onClick.AddListener(delegate
         {
-            AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
-            AndroidJavaObject myObject = new AndroidJavaObject("com.khlee.androidview.MyTest", unityActivity);
-            myObject.Call("showToast", "hello");
+            mViewController.Call("show");
         });
     }
 }
